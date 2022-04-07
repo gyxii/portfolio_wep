743ad9627775acbee3eda5bf2b79f34619d5ae69
gyxii <gyeongas383@gmail.com>
2022-04-08 01:25:48 +0900

    css 수정

--- js/script.js
+++ js/script.js
@@ -1,21 +1,73 @@
-const body = document.querySelector('body');
-const modal = document.querySelector('.modal');
-const btnOpenPopup = document.querySelector('.btn-open-popup');
-
-btnOpenPopup.addEventListener('click', () => {
-  modal.classList.toggle('show');
-
-  if (modal.classList.contains('show')) {
-    body.style.overflow = 'hidden';
+$(function(){
+  $("#confirm").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+      
+      //컨펌 이벤트 처리
+  });
+  $("#modal-open").click(function(){        
+      $("#popup").css('display','flex').hide().fadeIn();
+      //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+  });
+  $("#close").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+  });
+  function modalClose(){
+      $("#popup").fadeOut(); //페이드아웃 효과
   }
 });
 
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.classList.toggle('show');
 
-    if (!modal.classList.contains('show')) {
-      body.style.overflow = 'auto';
-    }
+$(function(){
+  $("#confirm").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+      
+      //컨펌 이벤트 처리
+  });
+  $("#modal-open1").click(function(){        
+      $("#popup1").css('display','flex').hide().fadeIn();
+      //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+  });
+  $("#close1").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+  });
+  function modalClose(){
+      $("#popup1").fadeOut(); //페이드아웃 효과
+  }
+});
+
+
+$(function(){
+  $("#confirm").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+      
+      //컨펌 이벤트 처리
+  });
+  $("#modal-open2").click(function(){        
+      $("#popup2").css('display','flex').hide().fadeIn();
+      //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+  });
+  $("#close2").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+  });
+  function modalClose(){
+      $("#popup2").fadeOut(); //페이드아웃 효과
+  }
+});
+
+$(function(){
+  $("#confirm").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+      
+      //컨펌 이벤트 처리
+  });
+  $("#modal-open3").click(function(){        
+      $("#popup3").css('display','flex').hide().fadeIn();
+      //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+  });
+  $("#close3").click(function(){
+      modalClose(); //모달 닫기 함수 호출
+  });
+  function modalClose(){
+      $("#popup3").fadeOut(); //페이드아웃 효과
   }
 });
