f1f2ef50f5b96ed01d1996b32d55c2aae3575336
gyxii <gyeongas383@gmail.com>
2022-04-08 14:17:11 +0900

    css 수정

--- js/script.js
+++ js/script.js
@@ -71,3 +71,43 @@ $(function(){
       $("#popup3").fadeOut(); //페이드아웃 효과
   }
 });
+
+
+
+$(function(){
+    $("#confirm").click(function(){
+        modalClose(); //모달 닫기 함수 호출
+        
+        //컨펌 이벤트 처리
+    });
+    $("#modal-open4").click(function(){        
+        $("#popup4").css('display','flex').hide().fadeIn();
+        //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+    });
+    $("#close4").click(function(){
+        modalClose(); //모달 닫기 함수 호출
+    });
+    function modalClose(){
+        $("#popup4").fadeOut(); //페이드아웃 효과
+    }
+  });
+  
+
+  $(function(){
+    $("#confirm").click(function(){
+        modalClose(); //모달 닫기 함수 호출
+        
+        //컨펌 이벤트 처리
+    });
+    $("#modal-open5").click(function(){        
+        $("#popup5").css('display','flex').hide().fadeIn();
+        //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
+    });
+    $("#close5").click(function(){
+        modalClose(); //모달 닫기 함수 호출
+    });
+    function modalClose(){
+        $("#popup5").fadeOut(); //페이드아웃 효과
+    }
+  });
+  
